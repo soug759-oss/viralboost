a2382222670f6718f23e9771ad53ea8bb66fc65b
soug759-oss <soug759@gmail.com>
2026-02-20 00:09:49 +0100

    Update and rename server (7).js to server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '.')));
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_VOTRE_CLE');
-const anthropic = new Anthropic({ apiKey: process.env.ANTHROPIC_API_KEY || 'VOTRE_CLE' });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'pk_test_51T2LGAJRrVNvN9TSGu2IB37Rn1Ib8J65TQ159AM7BGwmAhBQRwoT6dNxPVeY8CTSZzqMmso1XMJx6LNYFCVSn4q000pEM01MTS');
+const anthropic = new Anthropic({ apiKey: process.env.ANTHROPIC_API_KEY || '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' });
 
 // ── MONGODB ──
 const MONGO_URI = process.env.MONGO_URI || '';
